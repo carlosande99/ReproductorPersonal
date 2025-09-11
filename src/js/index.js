e6ab5c04addd35fc183eb1baa98d1c88e6d65282
carlosande99 <carlosandemetrioguerrero@gmail.com>
2025-09-11 12:45:45 +0200

    añadido svg de borrar y añadido mandar solicitud sin recargar pagina

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,21 @@ const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const songName = document.getElementById('song-name');
-
+const form = document.getElementById('form');
 const songs = [
   
 ];
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const url = document.getElementById('url').value;
+    try {
+        await fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ url })
+        });
+    } catch (err) {
+        console.error("Error en la petición:", err);
+    }
+});
