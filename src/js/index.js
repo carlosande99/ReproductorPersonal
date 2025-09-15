70f83a54dcfa605b11a9f1376712deb9363d09f0
carlosande99 <carlosandemetrioguerrero@gmail.com>
2025-09-15 12:38:35 +0200

    añadido funcionalidad a la tabla y inserccion de datos

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const songName = document.getElementById('song-name');
 const form = document.getElementById('form');
-const list = document.getElementById('playlist');
+const list = document.getElementById('songList');
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -25,9 +25,58 @@ fetch("/songs")
     .then(serverData => {
         console.log("Datos recibidos del servidor:", serverData);
         serverData.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item.datos.artist;
-            list.appendChild(li);
+            const tr = document.createElement('tr');
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+            const td3 = document.createElement('td');
+            const td4 = document.createElement('td');
+            const div = document.createElement('div');
+
+            const img = document.createElement('img');
+            const title = document.createElement('span');
+            const artist = document.createElement('span');
+            
+            img.src = item.datos.caractula;
+
+            td1.textContent = item.id;
+
+            title.textContent = item.datos.title;
+
+            artist.textContent = item.datos.artist;
+
+            td3.textContent = item.datos.album;
+            td4.textContent = calcularDuracion(item.datos.duracion);
+
+            td2.appendChild(img);
+            div.appendChild(title);
+            div.appendChild(artist);
+            td2.appendChild(div);
+
+            tr.onclick = () => {
+                console.log("Reproduciendo:", item.datos.title);
+            };
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            list.appendChild(tr);
         })
     })
     .catch(err => console.error("Error al obtener las canciones:", err));
+
+// pasa de ms a mm:ss
+function calcularDuracion(duracion) {
+     // Convertir a segundos totales
+  let totalSeconds = Math.floor(duracion / 1000);
+
+  // Calcular minutos y segundos
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+
+  // Formatear con 2 dígitos
+  let minStr = String(minutes).padStart(2, '0');
+  let secStr = String(seconds).padStart(2, '0');
+
+  return `${minStr}:${secStr}`;
+}
